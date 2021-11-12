eff82729d9b62e6cbb0566823b07701712dc0caa
João Paulo <joaopauloribeiropontes@gmail.com>
2021-11-12 02:57:05 -0300

    build.sh run.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,3 @@ cp .env.example .env
 php artisan key:generate
 git submodule init
 git submodule update
-cp laradock.env laradock/.env
-cd laradock
-sudo docker-compose up -d nginx mysql mongo
---
+++ run.sh
@@ -0,0 +1,3 @@
+cp laradock.env laradock/.env
+cd laradock
+sudo docker-compose up -d nginx mysql mongo
