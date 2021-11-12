2d9a049faf9ee53cfe3ad78965c5b4bad0cfad36
João Paulo <joaopauloribeiropontes@gmail.com>
2021-11-12 02:44:47 -0300

    build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
+composer install
 cp .env.example .env
 php artisan key:generate
+git submodule init
 cp laradock.env laradock/.env
 cd laradock
 sudo docker-compose up -d nginx mysql mongo
-php artisan migrate --seed
